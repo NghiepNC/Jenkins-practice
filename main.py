b8d151479ec0a3ee76fc8996374aa22ca9ea9436
NghiepNC <nha.y.thienztu9@gmail.com>
2023-04-06 23:41:19 +0700

    add header2

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   hello.py
+@Time    :   2023/04/06 23:16:06
+@Author  :   NghiepNC
+@Version :   0.1
+@Contact :   nguyenchinhnghiep8899@gmail.com
+@Desc    :   MLOps Flows
+'''
+
+class Video():
+    def __init__(self) -> None:
+            super().__init__()
+
+
+print('hello')
+print('hello jenkins')
