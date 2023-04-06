b137915f9c6d81e3e821de6b394af9bfb1cdac3d
NghiepNC <nha.y.thienztu9@gmail.com>
2023-04-06 23:16:29 +0700

    add header

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print(hello)
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   hello.py
+@Time    :   2023/04/06 23:16:06
+@Author  :   NghiepNC
+@Version :   0.1
+@Contact :   nguyenchinhnghiep8899@gmail.com
+@Desc    :   MLOps Flows
+'''
+
+print('hello')
+print('hello')
